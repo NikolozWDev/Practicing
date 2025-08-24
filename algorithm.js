5936a02a31c893e40dd34046d66942c41fe11cd9
Nikoloz Gigiashvili <gigiashvilinikoloz@gmail.com>
2025-08-25 00:02:55 +0400

    function, smalles sorting

--- algorithm.js
+++ algorithm.js
@@ -1,3 +1,30 @@
+function quickSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  
+  const pivot = arr[Math.floor(arr.length / 2)];
+  const left = [];
+  const right = [];
+  const equal = [];
+
+  for (let el of arr) {
+    if (el < pivot) {
+      left.push(el);
+    } else if (el > pivot) {
+      right.push(el);
+    } else {
+      equal.push(el);
+    }
+  }
+
+  return [...quickSort(left), ...equal, ...quickSort(right)];
+}
+
+console.log(quickSort([3, 6, 8, 10, 1, 2, 1]));
+
+
+
 double someNumber = .123 + .456 * Math.Pow(Math.E, .789 * Math.Pow((homeIndex + .22), .012));
 algorithm that shows someNumber
 
