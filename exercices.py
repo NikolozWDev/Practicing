a03e8df8c5b6c8d64e95584b59af5e2d36be2d00
Nikoloz Gigiashvili <gigiashvilinikoloz@gmail.com>
2025-09-20 00:02:25 +0400

    python with shell

--- exercices.py
+++ exercices.py
@@ -1,3 +1,16 @@
+#!/bin/bash
+playPass () {
+    # your code
+}
+playPass "$1" "$2"
+import codewars_test as test
+from solution import play_pass
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases()
+
 F(0) = 1
 M(0) = 0
 F(n) = n - M(F(n - 1))
