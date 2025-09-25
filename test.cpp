e060bd31b5e085e18bf92cf114956fffc003e17e
Nikoloz Gigiashvili <gigiashvilinikoloz@gmail.com>
2025-09-25 22:19:18 +0400

    basic cpp code

--- test.cpp
+++ test.cpp
@@ -5,3 +5,16 @@ class PlayPass
   public:
   static std::string playPass(const std::string &s, int n);
 };
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!";
+  return 0;
+}
+
+for (int i = 0; i < 5; i++) {
+  cout << i << "\n";
+}
