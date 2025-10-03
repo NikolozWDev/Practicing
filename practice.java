e37fef965cf122a612427677574baaf60e02f5ca
Nikoloz Gigiashvili <gigiashvilinikoloz@gmail.com>
2025-10-04 00:02:03 +0400

    Java task

--- practice.java
+++ practice.java
@@ -1,3 +1,28 @@
+class SolutionTest {
+  
+    @Test
+    void testSomething() {
+        assertTrue(Kata.validateHello("hello"));
+        assertTrue(Kata.validateHello("ciao bella!"));
+        assertTrue(Kata.validateHello("salut"));
+        assertTrue(Kata.validateHello("hallo, salut"));
+        assertTrue(Kata.validateHello("hombre! Hola!"));
+        assertTrue(Kata.validateHello("Hallo, wie geht\'s dir?"));
+        assertTrue(Kata.validateHello("AHOJ!"));
+        assertTrue(Kata.validateHello("czesc"));
+        assertTrue(Kata.validateHello("Ahoj"));
+        assertFalse(Kata.validateHello("meh"));
+    }
+}
+
+public class Kata{
+  public static boolean validateHello(String greetings){
+    //Your code
+    return false;
+  }
+  
+}
+
 int sum = 0;
 for (int i = 1; i <= 5; i++) {
   sum = sum + i;
