aaf318ff3f94b948bbfe39884909c8acccb32582
Nikoloz Gigiashvili <gigiashvilinikoloz@gmail.com>
2025-11-04 15:07:35 +0400

    made python algorithm

--- exercices.py
+++ exercices.py
@@ -1,3 +1,13 @@
+import codewars_test as test
+from solution import *
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(FileNameExtractor.extract_file_name("1231231223123131_FILE_NAME.EXTENSION.OTHEREXTENSION"),"FILE_NAME.EXTENSION")
+        test.assert_equals(FileNameExtractor.extract_file_name("1_FILE_NAME.EXTENSION.OTHEREXTENSIONadasdassdassds34"),"FILE_NAME.EXTENSION")
+
 
 import codewars_test as test
 from preloaded import validate
