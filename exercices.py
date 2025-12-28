485d0582ea5d49bd4f39f9fd187505826cd6e5ff
Nikoloz Gigiashvili <gigiashvilinikoloz@gmail.com>
2025-12-29 01:06:08 +0400

    lost lineup

--- exercices.py
+++ exercices.py
@@ -1,4 +1,17 @@
 import codewars_test as test
+from solution import find_lineup
+
+@test.describe("Example Tests")
+def example_tests():
+    
+    @test.it("Valid Cases") 
+    def run_tests():
+        
+        test.assert_equals(find_lineup([1, 2, 0]), [3, 1, 2])
+        test.assert_equals(find_lineup([1, 4, 2, 6, 8, 0, 5, 7, 3]), [6, 1, 3, 9, 2, 7, 4, 8, 5])
+        test.assert_equals(find_lineup([0]), [1])
+
+import codewars_test as test
 from solution import max_fork
 
 @test.describe("Example Tests")
