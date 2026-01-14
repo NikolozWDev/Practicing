1e68726b9bcedeb71bcd118ded03978afddd1869
Nikoloz Gigiashvili <gigiashvilinikoloz@gmail.com>
2026-01-14 16:02:30 +0400

    split string task create

--- algorithm.js
+++ algorithm.js
@@ -1,3 +1,13 @@
+const { assert } = require('chai');
+
+describe("Split Strings", () => {
+  it("Basic tests", () => {
+    assert.deepEqual(solution("abcdef"), ["ab", "cd", "ef"]);
+    assert.deepEqual(solution("abcdefg"), ["ab", "cd", "ef", "g_"]);
+    assert.deepEqual(solution(""), []);
+  });
+});
+
 const { assert } = require("chai");
 
 describe("Sample Tests", () => {
