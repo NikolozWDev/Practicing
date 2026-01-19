28f4e9d3b5887533210fafa7a4e58ba72dd18037
Nikoloz Gigiashvili <gigiashvilinikoloz@gmail.com>
2026-01-19 18:14:56 +0400

    found js problem

--- findProblem.js
+++ findProblem.js
@@ -1,3 +1,15 @@
+const Test = require('@codewars/test-compat');
+
+describe("Tests", () => {
+  it("test", () => {
+var game = new SnakesLadders();
+Test.assertEquals(game.play(1, 1), "Player 1 is on square 38", "Should return: 'Player 1 is on square 38'");
+Test.assertEquals(game.play(1, 5), "Player 1 is on square 44", "Should return: 'Player 1 is on square 44'");
+Test.assertEquals(game.play(6, 2), "Player 2 is on square 31", "Should return: 'Player 2 is on square 31'");
+Test.assertEquals(game.play(1, 1), "Player 1 is on square 25", "Should return: 'Player 1 is on square 25'");
+  });
+});
+
 const { assert } = require('chai');
 
 describe("Silly addition", () => {
