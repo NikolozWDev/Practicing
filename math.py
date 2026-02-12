abf7e2a308b6a4cd012f5f3f0f99b89db51ee2a6
Nikoloz Gigiashvili <gigiashvilinikoloz@gmail.com>
2026-02-13 00:02:32 +0400

    how much coffee do you need

--- math.py
+++ math.py
@@ -1,3 +1,14 @@
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(how_much_coffee([]), 0)
+        test.assert_equals(how_much_coffee(['cw']), 1)
+        test.assert_equals(how_much_coffee(['CW']), 2)
+        test.assert_equals(how_much_coffee(['cw','CAT']), 3)
+        test.assert_equals(how_much_coffee(['cw','CAT','DOG']), 'You need extra sleep')
+        test.assert_equals(how_much_coffee(['cw','CAT', 'cw=others']), 3)
+
 # optimize
 def find_x(n):
     x = 0
