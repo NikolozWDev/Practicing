65de3377637d954469daf34fca8f4bce67875892
Nikoloz Gigiashvili <gigiashvilinikoloz@gmail.com>
2026-02-18 01:06:17 +0400

    example task

--- taskmanager.py
+++ taskmanager.py
@@ -1,3 +1,18 @@
+def example_tests():
+    maze = [[1,1,1,1,1,1,1],
+            [1,0,0,0,0,0,3],
+            [1,0,1,0,1,0,1],
+            [0,0,1,0,0,0,1],
+            [1,0,1,0,1,0,1],
+            [1,0,0,0,0,0,1],
+            [1,2,1,0,1,0,1]]
+    
+    @test.it("Should return Finish")
+    def example_test_case1():
+        test.assert_equals(maze_runner(maze,["N","N","N","N","N","E","E","E","E","E"]), "Finish")
+        test.assert_equals(maze_runner(maze,["N","N","N","N","N","E","E","S","S","E","E","N","N","E"]), "Finish")
+        test.assert_equals(maze_runner(maze,["N","N","N","N","N","E","E","E","E","E","W","W"]), "Finish")
+
 @test.describe('tests')
 def _():
     @test.it('sample tests')
