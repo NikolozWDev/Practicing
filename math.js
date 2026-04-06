f597f8fd750134f8b7df7d163260be9abce3d852
Nikoloz Gigiashvili <gigiashvilinikoloz@gmail.com>
2026-04-06 22:08:34 +0400

    math problem solved

--- math.js
+++ math.js
@@ -1,3 +1,23 @@
+const {assert} = require('chai')
+
+describe('Fixed tests', function () {
+
+  describe('Examples', function () {
+    it('Single raffle: 1 ticket out of 3', function () {
+      assert.strictEqual(raffleOdds([3], [1]), '1/3');
+    });
+
+    it('Two raffles: 1 ticket each out of 4', function () {
+      assert.strictEqual(raffleOdds([4, 4], [1, 1]), '7/16');
+    });
+
+    it('Three raffles with different pool sizes', function () {
+      assert.strictEqual(raffleOdds([2, 3, 6], [1, 1, 1]), '13/18');
+    });
+  });
+  
+});
+
 // Advanced prime number generator using segmented sieve and wheel factorization
 function generatePrimes(limit) {
     if (limit < 2) return [];
