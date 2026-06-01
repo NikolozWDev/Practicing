677831aa19919526b78236eb80ba9e555526fea3
Nikoloz Gigiashvili <gigiashvilinikoloz@gmail.com>
2026-06-01 21:14:38 +0400

    bmi function and weights/heights

--- todo.py
+++ todo.py
@@ -1,3 +1,14 @@
+def bmi(weight, height):
+    bmi = weight / height * height
+    if bmi <= 18.5:
+        return "Underweight"
+    elif bmi <= 25.0:
+        return "Normal"
+    elif bmi <= 30.0:
+        return "Overweight"
+    elif bmi > 30:
+        return "Obese"
+
 import threading
 import math
 import random
