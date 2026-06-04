ca9f4d5b9700e9d64dfbcd33dc61a7d073e5930e
Nikoloz Gigiashvili <gigiashvilinikoloz@gmail.com>
2026-06-05 00:42:40 +0400

    tested codewars task

--- codeTests.py
+++ codeTests.py
@@ -1,3 +1,17 @@
+import codewars_test as test
+from solution import solve
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(solve("abba"),"OK")
+        test.assert_equals(solve("abbaa"),"remove one")
+        test.assert_equals(solve("abbaab"),"not possible")
+        test.assert_equals(solve("madmam"),"remove one")
+        test.assert_equals(solve("raydarm"),"not possible")
+        test.assert_equals(solve("hannah"),"OK")
+
 import codewars
 @test.describe("Thanos Sort")
 def test_thanos():
